54316c808f6fac329ebf543ea31560a6f2dc6536
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-02-25 20:45:03 -0300

    Update vinculos.java

--- vinculos.java
+++ vinculos.java
@@ -6,24 +6,24 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 
 public class vinculos {
-    public static void main(String[] args) {
-        class vincular {
+    public static void vinculo_conta_cliente() {
+        try  {
+            Connection connection = conexao.getConnection();
 
-            public void vinculo_conta_cliente() {
-                try {
-                    Connection connection = conexao.getConnection();
 
-                    String sql = "SELECT * FROM clientes";
-                    PreparedStatement sintaxe = connection.prepareStatement(sql);
-
-                    ResultSet query = sintaxe.executeQuery();
-                    if (query.next()) {
-                        System.out.println("ID do cliente: " + query.getInt(1));
-                    }
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
+            String sql = "SELECT id FROM clientes where id = ?";
+            try (PreparedStatement consultando_id = connection.prepareStatement(sql)) {
+                consultando_id.setString(1, nome);
             }
+
+            PreparedStatement sintaxe = connection.prepareStatement(sql);
+
+            ResultSet query = sintaxe.executeQuery();
+            if (query.next()) {
+                System.out.println("ID do cliente: " + query.getInt(1));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 }
