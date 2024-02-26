cc52dc1776c93215ca75c28198b2dbd9126a8468
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-02-25 21:41:29 -0300

    Update clientes.java

--- clientes.java
+++ clientes.java
@@ -8,7 +8,13 @@ import java.util.Scanner;
 
 public class clientes {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
+    }
+
+    public static void criar_cliente() {
+        try {
+            Connection connection = conexao.getConnection();
+
+            Scanner sc = new Scanner(System.in);
             System.out.println("Digite seu nome: ");
             String nome = sc.nextLine();
             System.out.println("Digite seu sexo: \n M para Masculino \n F para Feminino \n O para outros");
@@ -16,25 +22,12 @@ public class clientes {
             System.out.println("Digite seu endereço: ");
             String endereco = sc.nextLine();
 
-            Connection connection = conexao.getConnection();
-
             String sql = "INSERT INTO clientes (nome,sexo,endereco) VALUES (?,?,?)";
             try (PreparedStatement inserindo_cliente = connection.prepareStatement(sql)) {
                 inserindo_cliente.setString(1, nome);
                 inserindo_cliente.setString(2, sexo);
                 inserindo_cliente.setString(3, endereco);
                 inserindo_cliente.executeUpdate();
-
-                String consulta_nome = "SELECT id FROM clientes WHERE nome = ? ";
-                try (PreparedStatement consultando_nome = connection.prepareStatement(consulta_nome);) {
-                    consultando_nome.setString(1, nome);
-                    ResultSet query = consultando_nome.executeQuery();
-                    if (query.next()) {
-                        System.out.println("O ID do seu perfil é: " + query.getInt(1));
-                    }
-
-                }
-
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -42,9 +35,24 @@ public class clientes {
             throw new RuntimeException();
         }
     }
-}
 
-//connection.prepareStatement(sql) literalmente prepara a sql
-//conectando com o banco de dados, utilizando o PreparedStatement,
-//que seria uma interface do java que basicamente precompila uma SQL
-//onde a variável statement recebe a SQL
+    public static void consultar_nome_cliente() {
+        try {
+            Connection connection = conexao.getConnection();
+
+            String consulta_nome = "SELECT id FROM clientes WHERE nome = ? ";
+            try (PreparedStatement consultando_nome = connection.prepareStatement(consulta_nome)) {
+                consultando_nome.setString(1, nome);
+                ResultSet query = consultando_nome.executeQuery();
+                if (query.next()) {
+                    System.out.println("O ID do seu perfil é: " + query.getInt(1));
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            } catch (SQLException ex) {
+            throw new RuntimeException();
+        }
+    }
+}
