3ebe6731c856d4c9e005ab79760e76e2606b7c93
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-02-25 21:42:04 -0300

    Update manipulacao.java

--- manipulacao.java
+++ manipulacao.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 public class manipulacao {
     public static void main(String[] args) {
 
-
-
+clientes.criar_cliente();
+clientes.consultar_nome_cliente();
+contas.criar_conta();
     }
 }
