53f85ca5b07ae5b906621cf30900ae0d3739ea5c
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-03 18:34:50 -0300

    Update Vendedor.java

--- Vendedor.java
+++ Vendedor.java
@@ -1,18 +1,15 @@
-package jdbc.example;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class vendedor {
+public class Vendedor {
     public static void main(String[] args) {
     }
 
     public static void criar_vendedor() {
         try {
-            Connection connection = conexao.getConnection();
+            Connection connection = Conexao.getConnection();
 
             Scanner sc = new Scanner(System.in);
             System.out.println("Digite o nome vendedor: ");
