3f69934dc5cd2df645deb83abe91d09ffaae4f5e
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-03-03 18:34:35 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,5 +1,3 @@
-package jdbc.example;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -8,17 +6,20 @@ import java.util.Scanner;
 public class Produto {
     public static void cadastrar_produto() {
 
-      int id_fornecedor = Vinculos.vinculo_produto_fornecedor();
+        int id_fornecedor = Vinculos.vinculo_produto_fornecedor();
 
         try (Scanner sc = new Scanner(System.in);
-             Connection connection = conexao.getConnection()) {
+             Connection connection = Conexao.getConnection()) {
 
             System.out.println("Qual é a descrição do produto?");
             String descricao = sc.nextLine();
+
             System.out.println("Qual o preço deste produto?");
             double preco = sc.nextDouble();
+
             System.out.println("Qual é o estoque deste produto?");
             int estoque = sc.nextInt();
+
 
             String sintaxe = "INSERT INTO produto (descricao,preco,estoque,id_fornecedor) VALUES (?,?,?,?)";
             try (PreparedStatement inserindo = connection.prepareStatement(sintaxe)) {
@@ -27,7 +28,10 @@ public class Produto {
                 inserindo.setInt(3, estoque);
                 inserindo.setInt(4, id_fornecedor);
 
-                 inserindo.executeUpdate();
+                inserindo.executeUpdate();
+
+            } catch (SQLException ex) {
+                throw new RuntimeException();
             }
 
         } catch (SQLException e) {
